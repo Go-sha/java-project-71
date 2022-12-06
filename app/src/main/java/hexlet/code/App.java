ac256ed3c776c8d58e247925ae89245651cc72aa
Georgii Samoilov <rez1jkeee@gmail.com>
2022-12-06 05:16:17 +0300

    step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.Differ;
-
 public class App {
     public static void main(String[] args) {
         Differ.generate(args);
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,18 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class Differ {
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
     public static void generate(String[] args) {
         int exitCode = new CommandLine(new Differ()).execute(args);
         System.exit(exitCode);
